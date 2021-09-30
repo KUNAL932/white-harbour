d14e705a29c0cecc51e67dfd761cba38ddada630
KUNAL932 <kunalkashyap932@gmail.com>
2021-09-30 20:42:30 +0530

    data schema changes added

--- backend/database_connection/connection.py
+++ backend/database_connection/connection.py
@@ -1,26 +1,28 @@
-from mongoengine import *
+from mongoengine import * 
+from mongoengine import Document, StringField , connect
 connect(db='white-harbour',host="localhost:27017")
 
+
 class User(Document):
-    username = StringField()
-    email = EmailField(required=True)
-    password = StringField(required=True,hidden=True)
-    create_at = DateTimeField()
+    username = StringField((max_length=50,required=False)
+    email = EmailField(required=True,max_length=50)
+    password = StringField(required=True,hidden=True,max_length=50)
+    create_at = DateTimeField(max_length=50)
     is_admin = BooleanField()
-    session_token = StringField(default=0)
 
-# class Product(Document):
-#     username = StringField(required=True)
-#     email = EmailField(required=True)
-#     password = PasswordField(required=True)
-#     create_at = DateTimeField(required=True)
-#     updated_at = DateTimeField(required=True)
+
+class Product(Document):
+    name = StringField(required=True,max_length=50)
+    description = StringField(required=True,max_length=100)
+    category = StringField(required=True)
+    price = StringField(required=True)
+    image = ImageField()
+    in_stock = BooleanField()
+    count = StringField()
+    create_at = DateTimeField()
     
-# class Category(Document):
-#     username = StringField(required=True)
-#     email = EmailField(required=True)
-#     password = PasswordField(required=True)
-#     create_at = DateTimeField(required=True)
-#     updated_at = DateTimeField(required=True)
 
-
+class Category(Document):
+    name = StringField(required=True)
+    description = StringField(required=True)
+    create_at = DateTimeField(required=False)
