a73a3f05a0fa3d423e7b101e4237eee73bb50d69
KUNAL932 <kunalkashyap932@gmail.com>
2021-09-30 19:49:02 +0530

    sigin and logout functionality modified with session

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template, request ,jsonify
+from flask import Flask, render_template, request ,jsonify, redirect, session, url_for,g
 from database_connection.insert_to_db import register_user
+from bson.objectid import ObjectId
+
 import random
 from database_connection.insert_to_db import user
-app = Flask(__name__)
-
 
 def generate_session_token(length=10):
     return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)]) for _ in range(length) ) 
+
+app = Flask(__name__)
+app.secret_key= generate_session_token()
+
 
 @app.route("/signup/",methods=["GET","POST"])
 def signup():
@@ -38,22 +42,17 @@ def signin():
         user_data = user.find_one({"email":email})
         if user_data:
             if user_data['password'] == password:
-                user_dict = user.find_one({"email":email})
-                user_dict.pop("password")
-                user_dict.pop("_id")
-                if user_data['session_token'] != 0:
-                    query ={ "$set": {"session_token":0}}
-                    user.update_one({"email":email},query)
-                    return jsonify({"error":"Previous Login Exists"})
-                session_token =  generate_session_token()
-                query ={ "$set": {"session_token":session_token} }
-                user.update_one({"email":email},query)
-                return jsonify({"token":session_token,"user":user_dict})
-
-                
-
-        return jsonify({"error":"Email Doesnot Exists, Register From Here"})
+                session.clear()
+                session['user'] = email
+                return redirect('/')
     return render_template('signin.html')
+
+
+@app.route("/logout/",methods=["GET"])
+def logout():
+    if request.method == "GET":
+        session.clear()
+        return redirect('/signin/')
 
 if __name__=="__main__":
     app.run(debug=True)
